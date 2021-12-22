cc4058052c73913f3f33eb64e5a02368d5c72d5d
thuan2172001 <thuan2172001@gmail.com>
2021-12-22 11:49:09 +0700

    init

---
+++ src/config/constant.ts
@@ -0,0 +1,5 @@
+const EXP = 30 * 24 * 60 * 60;
+const ALLOWED_IMAGE_TYPES = ['jpg', 'jpeg', 'png', 'gif'];
+const ALLOWED_IMAGE_SIZE = 30;
+
+export { ALLOWED_IMAGE_SIZE, ALLOWED_IMAGE_TYPES, EXP };
