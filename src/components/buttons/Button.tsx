d3f0b096bdb14845a8f7cfac887abb8ecee7792c
thuan2172001 <thuan2172001@gmail.com>
2022-01-17 10:58:27 +0700

    fix: custom button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -5,15 +5,21 @@ import { ImSpinner2 } from 'react-icons/im';
 enum ButtonVariant {
   'primary',
   'outline',
-  'ghost',
-  'light',
-  'dark',
+}
+
+enum ButtonColor {
+  'red',
+  'orange',
+  'blue',
+  'yellow',
+  'gray',
+  'purple',
 }
 
 type ButtonProps = {
   isLoading?: boolean;
-  isDarkBg?: boolean;
   variant?: keyof typeof ButtonVariant;
+  color?: keyof typeof ButtonColor;
 } & React.ComponentPropsWithoutRef<'button'>;
 
 export default function Button({
@@ -22,10 +28,34 @@ export default function Button({
   disabled: buttonDisabled,
   isLoading,
   variant = 'primary',
-  isDarkBg = false,
+  // color = "blue",
   ...rest
 }: ButtonProps) {
   const disabled = isLoading || buttonDisabled;
+
+  // danger (red - white)
+  // alert (yellow - black)
+
+  const initArgs = (): Array<string> => {
+    switch (variant) {
+      case 'primary':
+        return [
+          'bg-primary-400 text-white',
+          'hover:bg-primary-500 hover:text-white',
+          'active:bg-primary-600',
+          'disabled:bg-primary-600 disabled:hover:bg-primary-600',
+        ];
+      case 'outline':
+        return [
+          'text-primary-500 border border-primary-500',
+          'hover:bg-primary-500 hover:text-white',
+          'active:bg-primary-600',
+          'disabled:bg-primary-600 disabled:hover:bg-primary-600',
+        ];
+      default:
+        return [];
+    }
+  };
 
   return (
     <button
@@ -37,40 +67,7 @@ export default function Button({
         'focus:outline-none',
         'shadow-sm',
         'duration-75 transition-colors',
-        [
-          variant === 'primary' && [
-            'bg-primary-400 text-white',
-            'border border-primary-500',
-            'hover:bg-primary-500 hover:text-white',
-            'active:bg-primary-600',
-            'disabled:bg-primary-600 disabled:hover:bg-primary-600',
-          ],
-          variant === 'outline' && [
-            'text-primary-500',
-            'border border-primary-500',
-            isDarkBg
-              ? 'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800'
-              : 'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-          ],
-          variant === 'ghost' && [
-            'text-primary-500',
-            'shadow-none',
-            isDarkBg
-              ? 'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800'
-              : 'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
-          ],
-          variant === 'light' && [
-            'bg-white text-dark ',
-            'border border-gray-300',
-            'hover:text-dark hover:bg-gray-100',
-            'active:bg-white/80 disabled:bg-gray-200',
-          ],
-          variant === 'dark' && [
-            'bg-gray-900 text-white',
-            'border border-gray-600',
-            'hover:bg-gray-800 active:bg-gray-700 disabled:bg-gray-700',
-          ],
-        ],
+        initArgs(),
         'disabled:cursor-not-allowed',
         isLoading &&
           'relative !text-transparent hover:!text-transparent !cursor-wait !transition-none'
@@ -81,9 +78,8 @@ export default function Button({
           className={clsx(
             '-translate-x-1/2 -translate-y-1/2 absolute left-1/2 top-1/2',
             {
-              'text-white': variant === 'dark' || variant === 'primary',
-              'text-black': variant === 'light',
-              'text-primary-500': variant === 'outline' || variant === 'ghost',
+              'text-white': variant === 'primary',
+              'text-primary-500': variant === 'outline',
             }
           )}
         >
